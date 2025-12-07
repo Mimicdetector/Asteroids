10aba9ce2f58b48f583c11842b60f4fcc0f100a9
Mimicdetector <gregbills2015@gmail.com>
2025-12-07 15:58:10 -0600

    Project Finished, need to add optional extensions

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
-import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
-
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 def main():
     # Initialize pygame
     pygame.init()
@@ -13,7 +16,18 @@ def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     # Game loop
     while True:
         # Log the game state
@@ -23,12 +37,26 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    log_event("asteroid_split")
+                    shot.kill()
+                    asteroid.split()
         # Fill the screen with black
         screen.fill("black")
-
+        for obj in drawable:
+            obj.draw(screen)
         # Refresh the display
+        
         pygame.display.flip()
-
+	
+        dt = clock.tick(60) / 1000.0
 if __name__ == "__main__":
     main()
